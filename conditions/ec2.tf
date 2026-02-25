319021d1b4e4bfadfc2bf2464cfe6c37d01c9d7b
Jai <jaipalreddy1703@gmail.com>
2026-02-25 10:21:58 +0530

    action

---
+++ conditions/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "example" {
+
+    ami = var.ami_id
+    # if it is dev environment t3.micro, otherwise t3.smal
+    instance_type = var.envi == "dev" ? "t3.micro" : "t3.small"
+    vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+    tags = var.ec2_tags
+
+}
+
+resource "aws_security_group" "allow_tls" {
+
+    name = var.sg_name
+    description = var.sg_desc
+
+    egress {
+        from_port = var.sg_from_port
+        to_port = var.sg_to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+        ipv6_cidr_blocks = [ "::/0" ]
+    }
+
+    ingress {
+        from_port = var.sg_from_port
+        to_port = var.sg_to_port
+        protocol = "-1"
+        cidr_blocks = var.cidr_blocks
+        ipv6_cidr_blocks = [ "::/0" ]       
+    }
+
+    tags = var.sg_tags
+  
+}
---
+++ conditions/variables.tf
@@ -0,0 +1,63 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+    description = "RHEL 9 image"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map
+    default = {
+        Name = "conditions-demo"
+        Project = "roboshop"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "sg_name" {
+    default = "allow all traffic"
+    type = string
+}
+
+variable "sg_desc" {
+    default = "sg for my terrform - allow all traffic"  
+}
+
+variable "sg_to_port" {
+    type = number 
+    default = 0
+}
+
+variable "sg_from_port" {
+    type = number 
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list 
+    default = ["0.0.0.0/0"]
+  
+}
+
+variable "sg_tags" {
+
+    type = map 
+    default = {
+        Name = "allow-all-terraform"
+        Project = "roboshop"
+        Terraform = "true"
+        Environment = "dev"
+    }
+  
+}
+
+variable "envi" {
+
+    default = "QA"
+  
+}
