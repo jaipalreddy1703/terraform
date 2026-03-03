2eaa9971e58a62612916802fea8ae737684aa98e
Jai <jaipalreddy1703@gmail.com>
2026-03-03 10:29:16 +0530

    action

--- locals-demo/locals.tf
+++ locals-demo/locals.tf
@@ -1,4 +1,12 @@
 locals {
     instance_name = "${var.name}-${var.envi}"
     instance_type = "t3.micro"
+
+    common_tags = {
+        Project = "roboshop"
+        Terraform = "true"
+        Environment = "dev"
+    }
+
+    ec2_final_tags = merge(local.common_tags, var.ec2_tags)
 }
